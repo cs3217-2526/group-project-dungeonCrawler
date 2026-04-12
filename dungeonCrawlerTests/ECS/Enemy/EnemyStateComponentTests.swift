b23cd016fd0c4e2beeebebd007e9719610768e7d
jannnice <e0969059@u.nus.edu>
2026-04-12 23:22:08 +1200

    fix: EnemyStateComponentTests

--- dungeonCrawlerTests/ECS/Enemy/EnemyStateComponentTests.swift
+++ dungeonCrawlerTests/ECS/Enemy/EnemyStateComponentTests.swift
@@ -12,26 +12,44 @@ import simd
 
 @MainActor
 final class EnemyStateComponentTests: XCTestCase {
+    
+    var state: EnemyStateComponent!
+    var customState: EnemyStateComponent!
+    var timidStrategy1: TimidStrategy!
+    var timidStrategy2: TimidStrategy!
+    
+    override func setUp() {
+        super.setUp()
+        state = EnemyStateComponent()
+        timidStrategy1 = TimidStrategy()
+        timidStrategy2 = TimidStrategy()
+        customState = EnemyStateComponent(strategy: timidStrategy1)
+    }
+    
+    override func tearDown() {
+        timidStrategy1 = nil
+        timidStrategy2 = nil
+        customState = nil
+        state = nil
+        super.tearDown()
+    }
 
     // MARK: - Default initialisation
 
     func testDefaultStrategyIsStandardStrategy() {
-        let state = EnemyStateComponent()
         XCTAssertTrue(state.strategy is StandardStrategy)
     }
 
     // MARK: - Custom initialisation
 
     func testCustomStrategyCanBePassedIn() {
-        let state = EnemyStateComponent(strategy: TimidStrategy())
-        XCTAssertTrue(state.strategy is TimidStrategy)
+        XCTAssertTrue(customState.strategy is TimidStrategy)
     }
 
     // MARK: - Mutation
 
     func testStrategyCanBeMutated() {
-        let state = EnemyStateComponent()
-        state.strategy = TimidStrategy()
+        state.strategy = timidStrategy2
         XCTAssertTrue(state.strategy is TimidStrategy)
     }
 }
